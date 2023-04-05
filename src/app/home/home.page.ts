92fa59d537f4302011e51eb6898a418d8004ff17
MelloNickolas <nickolasphmello10@gmail.com>
2023-04-05 08:58:54 -0300

    id personalization

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -16,34 +16,34 @@ export class HomePage {
 
   public contacts: ContactsList[] = [
     {
-      id: '(11) 99527-1252',
+      id: 'ID: 1',
       name: 'Neymar Jr',
       image: 'https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2021/06/21995_47022457A67E1FF5.jpg?w=1200&h=1200&crop=1',
       email: 'neymarJr@gmail.com',
     },
     {
-      id: '(81) 9876-5432',
-      name: 'Maria',
-      image: 'https://ionicframework.com/docs/img/demos/avatar.svg',
-      email: 'maria@hotmail.com',
+      id: 'ID: 2',
+      name: 'Cristiano Ronaldo',
+      image: 'https://www.ofutebolero.com.br/__export/1655483775252/sites/elfutboleromx/img/2022/06/17/cristiano.jpg_1535001796.jpg',
+      email: 'cristianoRonaldo@gmail.com',
     },
     {
-      id: '(11) 1234-5678',
-      name: 'Pedro',
-      image: 'https://media.istockphoto.com/photos/portrait-of-handsome-man-picture-id1245419427',
-      email: 'pedro@yahoo.com',
+      id: 'ID: 3',
+      name: 'Lionel Messi',
+      image: 'https://pbs.twimg.com/media/FCFt02kVgAAdxrP.jpg:large',
+      email: 'lionelMessi@gmail.com',
     },
     {
-      id: '(21) 9999-8888',
-      name: 'Juliana',
-      image: 'https://media.istockphoto.com/photos/happy-woman-laughing-outdoors-picture-id1154090429',
-      email: 'juliana@gmail.com',
+      id: 'ID: 4',
+      name: 'Haaland',
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT72saTsf0IYg7WVhkB37Vfzc7QCoUDEaw8lC_F0Ef_VYkLhEaRau69UBdloNfsuMCdSCQ&usqp=CAU',
+      email: 'haaland@gmail.com',
     },
     {
-      id: '(31) 5555-4444',
-      name: 'Carlos',
-      image: 'https://ionicframework.com/docs/img/demos/avatar.svg',
-      email: 'carlos@outlook.com',
+      id: 'ID: 5',
+      name: 'Mbappe',
+      image: 'https://www.elfutbolero.us/__export/1636845948570/sites/elfutboleromx/img/2021/11/13/team_of_france_v_kazakhstan_-_2022_fifa_world_cup_qualifier.jpeg_2062789929.jpeg',
+      email: 'mbappe@gmail.com',
     },
   ];
 
